7717b44f2910569fdcacc82783ea5110705335ac
Tjaž Eržen <erzen.tjaz@gmail.com>
2021-05-08 19:27:35 +0200

    Prva definicija razredov Vozlisce, Povezava, Graf, Model.

---
+++ model.py
@@ -0,0 +1,13 @@
+from graf import Graph, Vertex, Edge
+
+class Model:
+    pass
+
+
+class Uporabnik: 
+    pass
+
+
+class Potovanje:
+    def __init__(self, zacetek, konec, cas_evidence):
+        self.cas_potovanja, self.pot = Graph.
