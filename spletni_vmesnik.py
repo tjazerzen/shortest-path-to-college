3ca9924618d6d1ce667b7da1bfe1989a4b03f2be
Tjaž Eržen <erzen.tjaz@gmail.com>
2021-05-14 21:01:25 +0200

    Odstanil balastno kodo in odvečne komentarje. Kjer je bilo potrebno, v kodo dodal dodatno razlago.

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -5,3 +5,10 @@ from model import Model, Uporabnik, Vozlisce
 IME_DATOTEKE = "podatki_grafov"
 moj_model = Model.iz_datoteke(IME_DATOTEKE)
 
+def nalozi_uporabnikovo_stanje():
+    uporabnisko_ime = "Tjaž Eržen"
+    if uporabnisko_ime:
+        try:
+            uporabnik = Uporabnik.iz_datoteke(uporabnisko_ime)
+        except FileNotFoundError:
+            print("Datoteke s takim imenom nisem našel.")
