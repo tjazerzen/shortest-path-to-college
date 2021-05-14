e47c1a2bc1578fe744fa2767e93808d99e9a4e3a
Tjaž Eržen <erzen.tjaz@gmail.com>
2021-05-14 21:39:33 +0200

    Lotil se uporabniškega spletnega vmesnika, namenjenega za več uporabnikov. Za enkrat se s piškoti ne ubadam.

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,14 +1,22 @@
 import bottle
 from datetime import date
-from model import Model, Uporabnik, Vozlisce
+from model import *
 
 IME_DATOTEKE = "podatki_grafov"
 moj_model = Model.iz_datoteke(IME_DATOTEKE)
 
 def nalozi_uporabnikovo_stanje():
-    uporabnisko_ime = "Tjaž Eržen"
+    uporabnisko_ime = "Tjaž Eržen" # TODO: Razvij funkcionalnost, da moj ime ne bo hardcodan v program
     if uporabnisko_ime:
         try:
             uporabnik = Uporabnik.iz_datoteke(uporabnisko_ime)
         except FileNotFoundError:
             print("Datoteke s takim imenom nisem našel.")
+            uporabnik = Uporabnik("Uporabnik brez imena")
+        return uporabnik
+    else:
+        bottle.redirect("/prijava/")
+
+def shrani_uporabnika(uporabnik: Uporabnik):
+    uporabnik.v_datoteko()
+
