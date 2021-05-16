3bbe37e72aa45840d0a232693b87f63a383a73a9
Tjaž Eržen <erzen.tjaz@gmail.com>
2021-05-16 20:14:53 +0200

    Počistil HTML balast

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -115,5 +115,10 @@ def isci():
     bottle.redirect("/")
 
 
+@bottle.post("/analiza-postajalisc/")
+def analiziraj_postajalisca():
+    uporabnik = trenutni_uporabnik()
+    bottle.redirect("/")
+
 
 bottle.run(debug=True, reloader=True)
