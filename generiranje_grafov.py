8ebc04ad11bacc7a13d10b03bb6d9df4a50f0e7b
Tjaž Eržen <erzen.tjaz@gmail.com>
2021-05-16 06:24:04 +0200

    Popravil branje iz datotek za razred Uporabnik

--- generiranje_grafov.py
+++ generiranje_grafov.py
@@ -2,30 +2,8 @@ from model import *
 
 model = Model.iz_datoteke(ime_datoteke="podatki_grafov.json")
 
-bled = Vozlisce("Bled")
-bohinjska_bistrica = Vozlisce("BohinjskaBistrica")
-kranj = Vozlisce("KranjAP")
-ljubljana_tivoli = Vozlisce("LjubljanaTivoli")
-fmf = Vozlisce("PostajaJadranska")
-tolmin = Vozlisce("Tolmin")
-kobarid = Vozlisce("Kobarid")
+andrej = Uporabnik.iz_datoteke("Andrej Jamar")
 
-graf2 = Graf(2)
+print(andrej)
 
-graf2.dodaj_tocke([bled, bohinjska_bistrica, kranj, ljubljana_tivoli, fmf, tolmin, kobarid])
-
-graf2.dodaj_neusmerjeno_povezavo(bled, bohinjska_bistrica)
-graf2.dodaj_neusmerjeno_povezavo(bled, ljubljana_tivoli)
-graf2.dodaj_neusmerjeno_povezavo(bled, kranj)
-graf2.dodaj_neusmerjeno_povezavo(kranj, ljubljana_tivoli)
-graf2.dodaj_neusmerjeno_povezavo(ljubljana_tivoli, tolmin)
-graf2.dodaj_neusmerjeno_povezavo(tolmin, kobarid)
-graf2.dodaj_neusmerjeno_povezavo(ljubljana_tivoli, fmf)
-
-print(graf2.dijkstra(tolmin, fmf), "\n")
-
-print(graf2)
-
-model.dodaj_nov_graf(graf2)
-
-model.v_datoteko()
+andrej.v_datoteko()
